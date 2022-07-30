0ad2015c8098f4216c674ce727e0c6916fd8c399
winston walter <winstonwalter07@gmail.com>
2022-07-30 19:40:26 -0400

    more stuff to the training class

--- Mind.py
+++ Mind.py
@@ -13,24 +13,35 @@
 # ================================================= |
 
 
-from numpy import array, exp
-
-
-class TrainingData:
-    def __init__(self, data: array, solution: array):
-        self.data = data
-        self.solution = solution
-    
+from numpy import array, exp, dot
 
 class Neurons:
     def __init__(self, value, weight, bias = None):
         self.value = value
         self.weight = weight
         self.bias = bias
+class Training:
+    def __init__(self, data: array, solution: array, neurons: Neurons):
+        self.data = data
+        self.solution = solution
+        self.neurons = neurons
+    
+    def train(self, epochs):
+        """this function will train with the data given when defining\
+            the Training class"""
+        for i in range(epochs):
+            input_layer = self.data
+            output = Operations.sigmoid(dot(input_layer, ))
+            error = output - self.solution
 
+            adjustment = error * Operations.sigmoid_deriv(output)
 
+            self.neurons.bias += dot(input_layer.T, adjustment)
+        
+        return self.neurons.bias
+
+        
 class Operations:
-
     def sigmoid(x):
         return 1 / (exp(-x))
 
@@ -50,5 +61,3 @@ class Operations:
     def error(target, output):
         return (target-output)
 
-
-print(Operations.reLu(100000))
